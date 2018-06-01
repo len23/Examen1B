0a71f88c573e781ee313e6b95bc66f2c2d034c06
Unknown <lenin.montalvo.23@hotmail.com>
2018-06-01 17:40:02 -0500

    imagenes

--- tienda-app/src/app/app.module.ts
+++ tienda-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CabeceraTiendaComponent } from './cabecera-tienda/cabecera-tienda.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraTiendaComponent
+    CabeceraTiendaComponent,
+    CarrouselComponent
   ],
   imports: [
     BrowserModule
