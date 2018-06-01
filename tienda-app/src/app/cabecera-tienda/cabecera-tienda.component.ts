c29d243966d5ae29e1e96b58601240da4fb2e2be
Unknown <lenin.montalvo.23@hotmail.com>
2018-06-01 17:22:41 -0500

    Cabecera Creada

---
+++ tienda-app/src/app/cabecera-tienda/cabecera-tienda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecera-tienda',
+  templateUrl: './cabecera-tienda.component.html',
+  styleUrls: ['./cabecera-tienda.component.css']
+})
+export class CabeceraTiendaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
