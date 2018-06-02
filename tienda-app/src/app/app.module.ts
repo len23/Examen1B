e4d2dc2c67b59f06c5adbf4ca0f73953d1295b42
Unknown <lenin.montalvo.23@hotmail.com>
2018-06-02 03:03:40 -0500

    Componente Detalles Tienda Creado

--- tienda-app/src/app/app.module.ts
+++ tienda-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CabeceraTiendaComponent } from './cabecera-tienda/cabecera-tienda.component';
 import { CarrouselComponent } from './carrousel/carrousel.component';
+import { DetallesTiendaComponent } from './detalles-tienda/detalles-tienda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraTiendaComponent,
-    CarrouselComponent
+    CarrouselComponent,
+    DetallesTiendaComponent
   ],
   imports: [
     BrowserModule
