8d714b1b2c467140ac3fda19842eae1427260498
Unknown <lenin.montalvo.23@hotmail.com>
2018-06-02 04:35:03 -0500

    Inputs mejorados

--- tienda-app/src/app/app.module.ts
+++ tienda-app/src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatInput} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+
 
 import { AppComponent } from './app.component';
 import { CabeceraTiendaComponent } from './cabecera-tienda/cabecera-tienda.component';
@@ -21,7 +24,9 @@ import { DetallesTiendaComponent } from './detalles-tienda/detalles-tienda.compo
     BrowserAnimationsModule,
     NoopAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
