6926b44fd066e9aba7afae5f0643e51763dacbfe
Unknown <lenin.montalvo.23@hotmail.com>
2018-06-02 03:55:11 -0500

    Revert "Inputs Puestos"
    This reverts commit 4fc5db138816d32ff7289a73b9e4b8127b28a9c8.

--- tienda-app/src/app/app.module.ts
+++ tienda-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
-import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { CabeceraTiendaComponent } from './cabecera-tienda/cabecera-tienda.component';
@@ -22,8 +21,7 @@ import { DetallesTiendaComponent } from './detalles-tienda/detalles-tienda.compo
     BrowserAnimationsModule,
     NoopAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule,
-    MatInputModule
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
